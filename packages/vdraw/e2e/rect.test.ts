ad4ac896acda0ba86c674876fef8477a6e3f390a
umaranis <mail@umaranis.com>
2025-04-29 19:45:57 +1000

    test: de-select by clicking on the canvas

--- packages/vdraw/e2e/rect.test.ts
+++ packages/vdraw/e2e/rect.test.ts
@@ -50,7 +50,7 @@ test('rectangle - select by clicking overlay', async ({ page }) => {
 // 	expect(rectCount).toBeGreaterThan(5);
 // });
 
-test('rectangle - de-select', async ({ page }) => {
+test('rectangle - de-select using escape key', async ({ page }) => {
 	await page.goto('/');
 	const canvas = getCanvasLocator(page);
 
@@ -67,6 +67,26 @@ test('rectangle - de-select', async ({ page }) => {
 
 	// de-select rect
 	await page.keyboard.press('Escape');
+	expect(canvas.locator('rect.selection-rect')).not.toBeVisible();
+});
+
+test('rectangle - de-select using canvas click', async ({ page }) => {
+	await page.goto('/');
+	const canvas = getCanvasLocator(page);
+
+	// no rectangles yet
+	await expect(canvas.locator('rect')).not.toBeVisible();
+
+	// add one rect
+	await canvas.click();
+	await expect(canvas.locator('rect:not(.overlay,.trace)')).toBeVisible();
+
+	// select rect
+	await canvas.locator('rect.overlay').click();
+	expect(canvas.locator('rect.selection-rect')).toBeVisible();
+
+	// de-select rect
+	await canvas.click({ position: { x: 500, y: 500 } });
 	expect(canvas.locator('rect.selection-rect')).not.toBeVisible();
 });
 
