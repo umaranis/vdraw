ba8b06d3eacf7fd9f2e38d422b6f1c2a386d9d76
umaranis <mail@umaranis.com>
2025-06-03 17:41:59 +1000

    feat: create overlay for selection corners

--- packages/vdraw/src/lib/view/selection/SelectionDecoratorRect.svelte
+++ packages/vdraw/src/lib/view/selection/SelectionDecoratorRect.svelte
@@ -5,6 +5,7 @@
 	}: { r: { x: number; y: number; width: number; height: number }; strokeWidth: number } = $props();
 </script>
 
+<!-- selection box -->
 <rect
 	x={r.x - strokeWidth / 2 + 2}
 	y={r.y - strokeWidth / 2 + 2}
@@ -12,10 +13,50 @@
 	height={r.height + strokeWidth / 2}
 	class="selection-rect"
 />
+
+<!-- top left -->
 <rect x={r.x - 6} y={r.y - 6} width={8} height={8} class="selection-corner" />
+<rect
+	x={r.x - 10}
+	y={r.y - 10}
+	width={16}
+	height={16}
+	class="selection-corner-overlay"
+	style="cursor: nwse-resize;"
+/>
+
+<!-- top right -->
 <rect x={r.x + r.width - 2} y={r.y - 6} width={8} height={8} class="selection-corner" />
+<rect
+	x={r.x + r.width - 6}
+	y={r.y - 10}
+	width={16}
+	height={16}
+	class="selection-corner-overlay"
+	style="cursor: nesw-resize;"
+/>
+
+<!-- bottom left -->
 <rect x={r.x - 6} y={r.y + r.height - 2} width={8} height={8} class="selection-corner" />
+<rect
+	x={r.x - 10}
+	y={r.y + r.height - 6}
+	width={16}
+	height={16}
+	class="selection-corner-overlay"
+	style="cursor: nesw-resize;"
+/>
+
+<!-- bottom right -->
 <rect x={r.x + r.width - 2} y={r.y + r.height - 2} width={8} height={8} class="selection-corner" />
+<rect
+	x={r.x + r.width - 6}
+	y={r.y + r.height - 6}
+	width={16}
+	height={16}
+	class="selection-corner-overlay"
+	style="cursor: nwse-resize;"
+/>
 
 <style>
 	.selection-rect {
@@ -27,5 +68,12 @@
 		stroke: #3182ed;
 		stroke-width: 1px;
 		fill: white;
+	}
+
+	.selection-corner-overlay {
+		stroke: #3182ed;
+		stroke-width: 1px;
+		fill: black;
+		opacity: 0;
 	}
 </style>
